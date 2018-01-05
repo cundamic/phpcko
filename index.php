b0e683bb7c9e0b67d08eaaad4acdc73700e6c0ee
Juraj Pecháč <jurajpechac@wade.local>
2018-01-05 08:35:21 +0100

    úvodné uloženie

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP s Yetim</title>
+</head>
+<body>
+ <h1>Programovanie s Yetim</h1>
+ <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia est tempore consectetur, illo a laudantium aperiam vero sequi mollitia, delectus magni quas, modi assumenda. Impedit cupiditate amet quaerat blanditiis voluptatibus!</p>
+ <p>Asperiores odio consequuntur rerum porro laboriosam sapiente perferendis quos. Quisquam molestias cumque blanditiis illo ea sequi! Voluptates sequi quae fuga dolore in qui quam laboriosam expedita, obcaecati doloremque tempore magni!</p>
+ <p>Asperiores sed rerum aperiam incidunt iste cumque commodi error nemo omnis consequatur excepturi, minima itaque architecto quis temporibus repellat adipisci ipsam. Labore ab aliquam distinctio tenetur, delectus deleniti sit commodi.</p>
+ <p>Dolores magnam autem, nesciunt, sit aperiam corporis nostrum neque, vero velit itaque laborum provident cum illum! Aperiam, quas dignissimos nesciunt, fugiat ipsum cum rem nihil neque doloremque earum dolorum a?</p>   
+</body>
+</html>
