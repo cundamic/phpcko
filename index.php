af8b9b6ef5760b680d97464ee658831b9bf96542
Juraj Pecháč <jurajpechac@wade.local>
2018-01-17 06:37:17 +0100

    video

--- index.php
+++ index.php
@@ -1,126 +1,66 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="kniznice/bootstrap/dist/css/bootstrap.min.css">
-<<<<<<< HEAD
-    
-  </head>
-  <block>
-  <div class="jumbotron jumbotron-fluid bg-info text-white text-center">
-  <div class="container">
-    <h1 class="display-3">Programovanie s Yetim</h1>    
-  </div>
-</div>
-<div class="container">
-<div class="card-deck">
-  <div class="card">
-    <div class="card-block">
-      <h4 class="card-title text-center">Ťahák pre Emmet</h4>
-      <div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="//www.youtube.com/embed/8g1X9T7CtD8" allowfullscreen=""></iframe>
-</div>
-
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-block">
-      <h4 class="card-title text-center">Premenné v PHP</h4>
-      <form action="">
-      <form>
-  <div class="form-group">
-    <label for="formGroupExampleInput">Premenná č.1</label>
-    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Hodnota prvej premennej">
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Premenná č.2</label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Hodnota druhej premennej">
-  </div>
-  <button type="submit" class="btn btn-info btn-block">Submit</button>
-</form>
-
-      </form>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-block">
-      <h4 class="card-title text-center">Odznak a pilulka</h4>
-      <div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="//www.youtube.com/embed/yyQDtHGSm-o" allowfullscreen=""></iframe>
-</div>
-
-    </div>
-  </div>
-</div>
-</div>
-=======
-  </head>
-  <body>
-    <div class="jumbotron jumbotron-fluid bg-primary text-white">
-      <div class="container">
-        <h1 class="display-3">Programovanie s Yetim</h1>                        
-      </div>
-    </div>
-    <div class="container">
-    <div class="row">
-      <div class="col-md-4">
-        <div class="card">
-        <img src="obrazky/duponoha.png" style="width:100%"  alt="">
-          <div class="card-block">
-            <h3 class="card-title">Okar Duponoha</h3>
-            <p class="card-text">
-Aj na obrov je Okar Duponoha príliš veľký a príliš pomalý. V exile, do ktorého sa dostal po tom, čo spôsobil príliš veľa náhodných škôd, túži po seberovnom priateľovi.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-block">
-            <h3 class="card-title text-center">Práca s premennými</h3>
-            <form action="">
-            <div class="form-group">
-              <label for="vstup1">vstup 1</label>
-              <input type="text" class="form-control" name="" id="vstup1" aria-describedby="helpId" placeholder="">
-              
-            </div>
-            <div class="form-group">
-              <label for="vstup2">vstup 2</label>
-              <input type="text" class="form-control" name="" id="vstup2" aria-describedby="helpId" placeholder="">
-            
-            </div>
-            <button type="sumbit" name="" id="" class="btn btn-info btn-lg btn-block">Odoslať</button>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card" >
-        <img src="obrazky/kryptak.png" style="width:100%" alt="">
-          <div class="card-block">
-          
-            <h3 class="card-title">Nalfar Krypták</h3>
-            <p class="card-text">
-Tmavý elf, ktorý totálne obdivuje kostry. Nalfar Krypták prirovnáva nekromanta ku psovi. "Len sa pozri na jeho očné jamky!"</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
->>>>>>> 8865d7e28dab238f5b0c233b0b6eeb079dd39442
-
-    <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="kniznice/jquery/dist/jquery.min.js"></script>
-    <script src="kniznice/tether/dist/js/tether.min.js"></script>
-    <script src="kniznice/bootstrap/dist/js/bootstrap.min.js"></script>
-<<<<<<< HEAD
-  </block>
-</html>
-=======
-    
-  </body>
-</html>
->>>>>>> 8865d7e28dab238f5b0c233b0b6eeb079dd39442
+ </html>
+ <html lang="en">
+   <head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="kniznice/bootstrap/dist/css/bootstrap.min.css">
+     
+   </head>
+   <block>
+   <div class="jumbotron jumbotron-fluid bg-info text-white text-center">
+   <div class="container">
+     <h1 class="display-3">Programovanie s Yetim</h1>    
+   </div>
+ </div>
+ <div class="container">
+ <div class="card-deck">
+   <div class="card">
+     <div class="card-block">
+       <h4 class="card-title text-center">Ťahák pre Emmet</h4>
+       <div class="embed-responsive embed-responsive-16by9">
+   <iframe class="embed-responsive-item" src="//www.youtube.com/embed/8g1X9T7CtD8" allowfullscreen=""></iframe>
+ </div>
+ 
+     </div>
+   </div>
+   <div class="card">
+     <div class="card-block">
+       <h4 class="card-title text-center">Premenné v PHP</h4>
+       <form action="">
+       <form>
+   <div class="form-group">
+     <label for="formGroupExampleInput">Premenná č.1</label>
+     <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Hodnota prvej premennej">
+   </div>
+   <div class="form-group">
+     <label for="formGroupExampleInput2">Premenná č.2</label>
+     <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Hodnota druhej premennej">
+   </div>
+   <button type="submit" class="btn btn-info btn-block">Submit</button>
+ </form>
+ 
+       </form>
+     </div>
+   </div>
+   <div class="card">
+     <div class="card-block">
+       <h4 class="card-title text-center">Odznak a pilulka</h4>
+       <div class="embed-responsive embed-responsive-16by9">
+   <iframe class="embed-responsive-item" src="//www.youtube.com/embed/yyQDtHGSm-o" allowfullscreen=""></iframe>
+ </div>
+ 
+     </div>
+   </div>
+ </div>
+ </div>
+ 
+     <!-- jQuery first, then Tether, then Bootstrap JS. -->
+     <script src="kniznice/jquery/dist/jquery.min.js"></script>
+     <script src="kniznice/tether/dist/js/tether.min.js"></script>
+     <script src="kniznice/bootstrap/dist/js/bootstrap.min.js"></script>
+   </block>
+ </html>
